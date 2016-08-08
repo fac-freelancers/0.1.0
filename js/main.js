46edf5a323ef7246e2f19180432a25264f2d7aa6
tu6619 <tu6619@my.bristol.ac.uk>
2016-08-08 20:19:08 +0100

    jquery for carousel

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
 $( document ).ready(function(){
+  /** for carousel */
+  $('.carousel.carousel-slider').carousel({full_width: true});
+  
     $(".button-collapse").sideNav({
         menuWidth: 300, // Default is 240
         edge: 'left', // Choose the horizontal origin
