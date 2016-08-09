dd6505396c98307b1fca70a6b06c0e9a2dca15d9
tu6619 <tu6619@my.bristol.ac.uk>
2016-08-09 18:32:11 +0100

    no change to this file

--- js/main.js
+++ js/main.js
@@ -1,7 +1,6 @@
 $( document ).ready(function(){
   /** for carousel */
   $('.carousel.carousel-slider').carousel({full_width: true});
-  
     $(".button-collapse").sideNav({
         menuWidth: 300, // Default is 240
         edge: 'left', // Choose the horizontal origin
